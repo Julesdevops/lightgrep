834dafcf3db6f2b103fdef0b35e4ba5b8ecf58be
julesdevops <julesbourdais7198@gmail.com>
2020-10-09 18:02:47 +0200

    refactor: extract core logic into lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,19 @@
 use std::env;
-use std::fs;
 use std::process;
+
+use lightgrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    
+
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
-
-    let contents = fs::read_to_string(config.filename)
-    .expect("Somehting went wrong reading the file");
     
-    println!("{}", contents);
-}
+    if let Err(e) = lightgrep::run(config) {
+        println!("Application error: {}", e);
 
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
