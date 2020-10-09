43b0dbd5922eea223a088f9af991345682f34a2e
julesdevops <julesbourdais7198@gmail.com>
2020-10-09 22:32:28 +0200

    doc: add comments where appropriate

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use std::fs;
 use std::env;
 use std::error::Error;
 
+// Struct that holds the arguments/options passed in
+// Also hold useful environment informations from env vars
 pub struct Config {
     pub query: String,
     pub filename: String,
@@ -9,29 +11,37 @@ pub struct Config {
 }
 
 impl Config {
-   pub fn new(args: &[String]) -> Result<Config, &'static str> {
+    // Validates arguments/options and return a new Config instance
+    // Or, return an error
+    pub fn new(args: &[String]) -> Result<Config, &'static str> {
         if args.len() < 3 {
             return Err("not enough arguments");
         }
 
+        //TODO refactor : don't use clone as it is not efficient
         let query = args[1].clone();
         let filename = args[2].clone();
 
+        //TODO style : change env var name to LGREP_CASE_INSENSITIVE
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
         
         Ok(Config { query, filename, case_sensitive })
     }
 }
 
+// Core business logic, find matches and display them
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    // Get file content as a String
     let contents = fs::read_to_string(config.filename)?;
 
+    // Get matches, case sensitive or else depending on config
     let results = if config.case_sensitive {
         search(&config.query, &contents)
     } else {
         search_case_insensitive(&config.query, &contents)
     };
 
+    // Display matches
     for line in results {
         println!("{}", line);
     }
@@ -39,9 +49,11 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+// Perform a case sensitive search
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
+    // TODO refactor
     for line in contents.lines() {
         if line.contains(query) {
             results.push(line);
@@ -51,10 +63,12 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+// Perform a case insensitive search
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
+    //TODO refactor
     for line in contents.lines() {
         if line.to_lowercase().contains(&query) {
             results.push(line);
@@ -64,6 +78,7 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     results
 }
 
+// Unit tests for search and search_case_insensitive methods
 #[cfg(test)]
 mod tests {
     use super::*;
--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,13 @@ use lightgrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
+    // Validate arguments and handle parsing errors
     let config = Config::new(&args).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
     
+    // Run the main logic and handle errors
     if let Err(e) = lightgrep::run(config) {
         eprintln!("Application error: {}", e);
 
